df6350eeab4dae58f5c84cf24662a6e39de2d5f9
Lydia Bell <lrbell84@gmail.com>
2021-07-13 01:33:23 +0000

    commit tree 3468ed0785c9ffc7cfefcf869f391b20f1001a31

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+const totalBatteries = batteryBatches.reduce(function(sum, collection) {
+    return collection + sum
+}
+
+)
+
 // Code your solution here
